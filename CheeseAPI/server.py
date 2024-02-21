0cd5689cb9a9c3bbe0a7759dd026329992038098
CheeseUnknown <cheese@cheese.ren>
2024-02-21 14:40:35 +0800

    推进 v0.3.13
    依赖更新：
    - 删除 CheeseType 依赖

--- CheeseAPI/server.py
+++ CheeseAPI/server.py
@@ -1,22 +1,19 @@
 import os
 
-from CheeseType import IPv4, Port, NonNegativeInt
-
 class Server:
     def __init__(self) -> None:
-        self.host: IPv4 = '0.0.0.0'
-        self.port: Port = 5214
-        self._workers: NonNegativeInt = 1
+        self.host: str = '0.0.0.0'
+        self.port: int = 5214
+        self._workers: int = 1
         self.static: str | None = None
         self.backlog: int = 128
 
     @property
-    def workers(self) -> NonNegativeInt:
+    def workers(self) -> int:
         return self._workers
 
     @workers.setter
     def workers(self, value):
-        value = NonNegativeInt(value)
         if value == 0:
             self._workers = os.cpu_count() * 2 + 1
         else:
